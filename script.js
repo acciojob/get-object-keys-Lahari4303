9fda946608d7854e0f95a15b75d0218c75025ed5
Lahari4303 <glaharireddy24@gmail.com>
2026-01-25 22:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let name ={
+	a : "Ram",
+	b : "Raju",
+	c : "Pooja",
+	d : "Lahari"
+}
+function getKeys(name){
+	for(let key in name){
+		return key
+	}
+}
+console.log(getKeys(name))
