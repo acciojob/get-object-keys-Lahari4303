7389219ce1d387987f9e050891d16b0e9c5b294d
Lahari4303 <glaharireddy24@gmail.com>
2026-02-17 10:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //your JS code here. If required.
 const student = {
   name: "John"
-};
+}
 function getKeys(student){
 	return Object.keys(student)
 }
+const person = {
+  name: "Alice",
+  age: 22,
+  city: "New York"
+};
+
+console.log(getKeys(person)); 
 console.log(getKeys(Student))
